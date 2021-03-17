0a0e66c1f052a21806eddd3f2841f1c7ae744561
Jay Shah <jayshah14js@gmail.com>
2021-03-17 16:40:50 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import Diploma from "./imageFiles/diploma.png";
+import Assessment from "./imageFiles/assessment.png";
+import ACLogo from "./imageFiles/ac-main-logo.png";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="nav">
+        <img src={ACLogo} />
+      </div>
+      <div className="main-title">
+        <h1>
+          <span>Choose your workflow.</span>
+        </h1>
+      </div>
+      <div className="workflow-container">
+        <div className="parent">
+          <div className="column">
+            <div className="image-wrapper">
+              <img src={Assessment} />
+            </div>
+            <div className="content-wrapper">
+              <h3 className="item1">
+                Invisalign<span className="sup">®</span> Go SAFE Assessment Form
+              </h3>
+            </div>
+            <div className="text-wrapper">
+              <p className="powered">
+                Powered by <span className="kiroku-logo">Kiroku</span>
+              </p>
+            </div>
+            <div className="text-wrapper">
+              <p>
+                This will guide you through the assessment of a patient, when
+                considering orthodontic treatment. This is available to previous
+                delegates of aligner consulting Invisalign
+                <span className="sup-small">®</span> Go courses.
+              </p>
+            </div>
+            <div className="button-container">
+              <button>Select</button>
+            </div>
+          </div>
+          <div className="column">
+            <div className="image-wrapper">
+              <img src={Diploma} />
+            </div>
+            <div className="content-wrapper">
+              <h3 className="item1">SAFE Diploma Assessment Form</h3>
+            </div>
+            <div className="text-wrapper">
+              <p className="powered">
+                Powered by <span className="kiroku-logo">Kiroku</span>
+              </p>
+            </div>
+            <div className="text-wrapper">
+              <p>
+                This will guide you though a comprehensive assessment of a
+                patient, when considering orthodontic treatment for a range of
+                complexities. This is available to previous and current SAFE
+                diploma students.
+              </p>
+            </div>
+            <div className="button-container">
+              <button>Select</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
